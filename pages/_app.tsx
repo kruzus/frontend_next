2a171472ae5ea736c05845d990963879e8946da2
Bek <kruzus@live.com>
2021-09-29 03:13:44 -0400

    fresh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.title = "Welcome";
+  });
   return <Component {...pageProps} />
 }
 export default MyApp
